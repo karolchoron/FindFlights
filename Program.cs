2348d1ebbcc52a50934636f524b8f44b927d2d16
karolchoron <karolchoron@gmail.com>
2025-02-19 20:59:32 +0100

    changes CORS

--- Program.cs
+++ Program.cs
@@ -6,9 +6,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) }); // BASE ADDRESS
-//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7220") }); // HTTPS LOCALHOST
-//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5240") }); // HTTP LOCALHOST
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://findflightsapiserver.onrender.com/") }); // Backend API server address
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5240") }); // localhost http: - 5240 ; https: 7220
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://findflightsapiserver.onrender.com") }); // Backend API server address
 
 await builder.Build().RunAsync();
